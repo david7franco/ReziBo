7f875da3cd121bbcb767fa048bb033a414de73fe
Shvang42 <Shivang.patel961@gmail.com>
2023-10-22 13:28:23 -0700

    login redirect
    added methods that can be used to redirect the user depending on what group they belong to, has not been implemented fully but the base code is there

--- accounts/views.py
+++ accounts/views.py
@@ -7,12 +7,35 @@ from .forms import TextEntryForm
 from django.contrib.auth.decorators import login_required
 from .models import Task
 
+from django.contrib.auth import authenticate, login as auth_login
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect(redirect_based_on_group(user))  # This line will now work as expected
+        else:
+            return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
+
+    elif request.user.is_authenticated:
+        return redirect(redirect_based_on_group(request.user))
+    else:
+        return render(request, 'registration/login.html')
+    
+    
+def redirect_based_on_group(user):
+    if not(user.is_superuser):
+        return '/'  # Direct all other users, including 'Vets' and 'Customers', to the home page
+    return '/admin'  
 
 @login_required
 def text_entry(request):
