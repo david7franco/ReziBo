b73f1fb92aabf87a66df1c3faaa5423ced0c1ab3
david7franco@Outlook.com <david7franco@Outlook.com>
2023-10-22 17:44:21 -0400

    fixed file structure to look at static css files

--- testproj/urls.py
+++ testproj/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")), 
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/', include('accounts.urls'))
-    
+    path('', include('accounts.urls')),
+
 ]
