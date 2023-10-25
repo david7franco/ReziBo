dcc05e8e652cafd173a385880ab66d868acc9902
Samuel Costantino <costan42@students.rowan.edu>
2023-10-25 19:14:56 -0400

    Task model has been further fleshed out

--- accounts/models.py
+++ accounts/models.py
@@ -10,16 +10,15 @@ class TextEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    task_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    floor = models.PositiveIntegerField(default=0)
-    # assingor = models.ForeignKey(User, on_delete=models.CASCADE)
-    assingor = models.CharField(max_length=200, default='Null')
+    floor = models.PositiveIntegerField()
+    assingor = models.CharField(max_length=200)
     description = models.TextField()
-    status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')], default=1)
-    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')], default=1)
-    date_posted = models.DateTimeField(default=datetime.now)
-    # image = models.ImageField(blank=True)
+    status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')])
+    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')])
+    date_posted = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True)
    
     def __str__(self):
         return self.title
