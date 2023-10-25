3e589bb022c15d84cc3958c828e5d4167060be0b
Samuel Costantino <costan42@students.rowan.edu>
2023-10-25 19:14:56 -0400

    Task model has been further fleshed out

--- accounts/models.py
+++ accounts/models.py
@@ -8,9 +8,15 @@ class TextEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  
 
 class Task(models.Model):
+    task_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
+    floor = models.PositiveIntegerField()
+    assingor = models.CharField(max_length=200)
     description = models.TextField()
     status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')])
+    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')])
+    date_posted = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True)
    
     def __str__(self):
         return self.title
