5342be42ff205fbeaa7393a79cf742cdeaa363b2
Samuel Costantino <costan42@students.rowan.edu>
2023-10-25 21:09:11 -0400

    troubleshooted models and added default values for most task fields; revised and successfully ran testing script

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User, AbstractUser, Group, Permission
+from django.utils.translation import gettext as _
 
 # Create your models here.
 class TextEntry(models.Model):
@@ -9,15 +10,16 @@ class TextEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  
 
 class Task(models.Model):
-    task_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    floor = models.PositiveIntegerField()
-    assingor = models.ForeignKey(User, on_delete=models.CASCADE)
+    floor = models.PositiveIntegerField(default=0)
+    # assingor = models.ForeignKey(User, on_delete=models.CASCADE)
+    assingor = models.CharField(max_length=200, default='Null')
     description = models.TextField()
     status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')])
-    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')])
-    date_posted = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(blank=True)
+    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')], default=1)
+    date_posted = models.DateTimeField(default=datetime.now)
+    # image = models.ImageField(blank=True)
    
     def __str__(self):
         return self.title
@@ -27,6 +29,14 @@ class CustomUser(AbstractUser):
     is_manager = models.BooleanField(default=False)
     room_number = models.PositiveIntegerField()
     phone_number = models.CharField(max_length=15, blank=True)
+
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='customuser_set'
+    )
 
 class ManagerUser(CustomUser):
     resident_assistants = models.TextField()
