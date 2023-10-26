d516221b3e86e1f1db4c0870d21ff9094109d16a
Samuel Costantino <costan42@students.rowan.edu>
2023-10-25 20:05:24 -0400

    fleshed out user models, may need further embellishment; made small change to assingor field in task model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 class TextEntry(models.Model):
@@ -11,7 +11,7 @@ class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     floor = models.PositiveIntegerField()
-    assingor = models.CharField(max_length=200)
+    assingor = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')])
     priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')])
@@ -20,3 +20,19 @@ class Task(models.Model):
    
     def __str__(self):
         return self.title
+    
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    room_number = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=15, blank=True)
+
+class ManagerUser(CustomUser):
+    resident_assistants = models.TextField()
+
+class RaUser(CustomUser):
+    floor = models.PositiveIntegerField()
+    manager = models.CharField(max_length=200)
+
+class ResidentUser(CustomUser):
+    floor = models.PositiveIntegerField()
