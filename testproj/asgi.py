64dc7c130c7a4d3f0731f35e7733a44122d077f5
Shvang42 <Shivang.patel961@gmail.com>
2023-11-02 22:11:45 -0400

    Code for chatroom
    does not work as of yet simply because of a few imports statements that do not work

--- testproj/asgi.py
+++ testproj/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for testproj project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproj.settings')
+from accounts import consumers
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [
+                path("ws/chat/<str:room_name>/", consumers.ChatConsumer),
+            ]
+        )
+    ),
+})
