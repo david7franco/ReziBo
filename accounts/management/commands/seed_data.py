b9050247c44510f498d7398c8163bce79d903b8e
david7franco@Outlook.com <david7franco@Outlook.com>
2023-11-02 21:28:00 -0400

    working on customUser bug

--- accounts/management/commands/seed_data.py
+++ accounts/management/commands/seed_data.py
@@ -1,18 +1,56 @@
 from django.core.management.base import BaseCommand
 from accounts.models import Task
-
-
+from accounts.models import RaUser
 class Command(BaseCommand):
     help = "Seed the database with test data"
 
     def handle(self, *args, **options):
     
-        Task.objects.create(title="Task 1", floor=1, assingor='Jane Doe', description="Sample task 1", status=1, priority=1)
-        Task.objects.create(title="Task 2", floor=2, assingor='John Doe', description="Sample task 2", status=2, priority=2)
-        Task.objects.create(title="Task 3", floor=3, assingor='DIO', description="Sample task 3", status=3, priority=3)
-        Task.objects.create(title="Task 4", floor=4, assingor='Jotaro Kujo', description="Sample task 4", status=4, priority=3)
-        Task.objects.create(title="Task 5", floor=5, assingor='Giorno Giovanna', description="This task contains some sample data", status=1, priority=3)
+        #Task.objects.create(title="Task 1", floor=1, assingor='Jane Doe', description="Sample task 1", status=1, priority=1)
+        #Task.objects.create(title="Task 2", floor=2, assingor='John Doe', description="Sample task 2", status=2, priority=2)
+        #Task.objects.create(title="Task 3", floor=3, assingor='DIO', description="Sample task 3", status=3, priority=3)
+        #Task.objects.create(title="Task 4", floor=4, assingor='Jotaro Kujo', description="Sample task 4", status=4, priority=3)
+        #Task.objects.create(title="Task 5", floor=5, assingor='Giorno Giovanna', description="This task contains some sample data", status=1, priority=3)
         # Task.objects.all().delete()
+        
+        # username = "johnsmith"
+        # first_name = "John"
+        # last_name = "Smith"
+        # email = "john.smith@example.com"
+        # room_number = 101
+        # phone_number = "123-456-7890"
+        # floor = 2
+        # manager_name = "ManagerName"
+
+        # # Create a RaUser instance
+        # RaUser.objects.create(
+        #     username=username,
+        #     first_name=first_name,
+        #     last_name=last_name,
+        #     email=email,
+        #     room_number=room_number,
+        #     phone_number=phone_number,
+        #     floor=floor,
+        #     manager=manager_name,
+        # )
+
+
+
+        # Create a RaUser instance
+        RaUser.objects.create(
+        username='janesmith',
+        password='admin@123',
+        first_name='Jane',
+        last_name='Smith',
+        email='jane.smith@example.com',
+        room_number=102,
+        phone_number='321-654-9870',
+        floor=1,
+        manager='ManagerName',
+        #FK_CustomUser_RaUser=custom_user  # Pass the CustomUser instance here
+        )
+
+        
 
         #Task.objects.create(title="Task 6", description="Sample task 1", status=1)
         self.stdout.write(self.style.SUCCESS("Successfully seeded test data."))
--- accounts/models.py
+++ accounts/models.py
@@ -3,54 +3,79 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser, Group, Permission
 from django.utils.translation import gettext as _
 
+
 # Create your models here.
 class TextEntry(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    TaskId = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     floor = models.PositiveIntegerField(default=0)
     # assingor = models.ForeignKey(User, on_delete=models.CASCADE)
-    assingor = models.CharField(max_length=200, default='Null')
+    assingor = models.CharField(max_length=200, default="Null")
     description = models.TextField()
-    status = models.IntegerField(choices=[(1, 'To Do'), (2, 'In Progress'), (3, 'On Hold'),  (4, 'Done')], default=1)
-    priority = models.IntegerField(choices=[(1, 'Low'), (2, 'Medium'), (3, 'High')], default=1)
+    status = models.IntegerField(
+        choices=[(1, "To Do"), (2, "In Progress"), (3, "On Hold"), (4, "Done")],
+        default=1,
+    )
+    priority = models.IntegerField(
+        choices=[(1, "Low"), (2, "Medium"), (3, "High")], default=1
+    )
     date_posted = models.DateTimeField(default=datetime.now)
     # image = models.ImageField(blank=True)
-   
+
     def __str__(self):
         return self.title
-    
-class CustomUser(AbstractUser):
-    room_number = models.PositiveIntegerField()
-    phone_number = models.CharField(max_length=15, blank=True)
 
-    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='customuser_set')
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name=_('user permissions'),
-        blank=True,
-        related_name='customuser_set'
-    )
 
-class ManagerUser(CustomUser):
-    is_admin = models.BooleanField(default=True)
-    is_r_a = models.BooleanField(default=False)
-    is_resident = models.BooleanField(default=False)
+#class CustomUser(models.Model):
+    # #customUserID = models.AutoField(primary_key=True)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # room_number = models.PositiveIntegerField()
+    # phone_number = models.CharField(max_length=15, blank=True)
+    # username = models.CharField(max_length=20, blank=True )
+    # first_name = models.CharField(max_length=50, blank=True )
+    # last_name = models.CharField(max_length=50, blank=True )
+    # email = models.EmailField(max_length=200, blank=True )
+
+    # groups = models.ManyToManyField(
+    #     Group, verbose_name=_("groups"), blank=True, related_name="customuser_set"
+    # )
+    # user_permissions = models.ManyToManyField(
+    #     Permission,
+    #     verbose_name=_("user permissions"),
+    #     blank=True,
+    #     related_name="customuser_set",
+    # )
+
+
+class AdminUser(models.Model):
+    #managerID = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     resident_assistants = models.TextField()
 
-class RaUser(CustomUser):
-    is_admin = models.BooleanField(default=False)
-    is_r_a = models.BooleanField(default=True)
-    is_resident = models.BooleanField(default=False)
+
+class RaUser(models.Model):
+    #RaID = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     floor = models.PositiveIntegerField()
     manager = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    room_number = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    username = models.CharField(max_length=20, blank=True )
+    first_name = models.CharField(max_length=50, blank=True )
+    last_name = models.CharField(max_length=50, blank=True )
+    email = models.EmailField(max_length=200, blank=True )
+    password = models.CharField(max_length=50)
 
-class ResidentUser(CustomUser):
-    is_admin = models.BooleanField(default=False)
-    is_r_a = models.BooleanField(default=False)
-    is_resident = models.BooleanField(default=True)
+
+class ResidentUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #resdidentID = models.AutoField(primary_key=True)
+    residentName = models.CharField(max_length=200)
     floor = models.PositiveIntegerField()
