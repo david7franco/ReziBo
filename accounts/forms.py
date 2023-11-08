52e5a6449537a13cf337c88b3584a2b449d59559
Samuel Costantino <costan42@students.rowan.edu>
2023-11-07 21:35:54 -0500

    included new fields in the createTicket forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,5 +23,5 @@ class TaskForm(forms.ModelForm):
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'floor', 'description']
+        fields = ['title', 'floor', 'description', 'image', 'file']
 
