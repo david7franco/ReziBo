87e6243b37c0ead7f807f5ee92be30e5c80a5d53
david7franco@Outlook.com <david7franco@Outlook.com>
2023-11-21 19:12:58 -0500

    linked tasks to RAuser and residentUser

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser, Group, Permission
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
-
 from django.utils import timezone
 from django.contrib.auth.models import User
-
 '''
 User = get_user_model()
 
@@ -30,44 +28,6 @@ class TextEntry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
-class Task(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
-    floor = models.PositiveIntegerField(default=0)
-    description = models.TextField()
-    ra = models.ForeignKey(User, related_name='ra_tasks', on_delete=models.SET_NULL, null=True, blank=True)
-    resident = models.ForeignKey(User, related_name='resident_tasks', on_delete=models.SET_NULL, null=True, blank=True)
-    status = models.IntegerField(
-        choices=[(1, "To Do"), (2, "In Progress"), (3, "On Hold"), (4, "Done")],
-        default=1,
-    )
-    priority = models.IntegerField(
-        choices=[(1, "Low"), (2, "Medium"), (3, "High")], default=1
-    )
-    date_posted = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(null=True, blank=True)
-    file = models.FileField(null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-class Comments(models.Model):
-    comment_id = models.AutoField(primary_key=True)
-    fk_task_comment = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
-    text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    date_posted = models.DateTimeField(default=datetime.now)
-
-
-class Annotations(models.Model):
-    annotate_id = models.AutoField(primary_key=True)
-    fk_task_annotations = models.ForeignKey(Task, related_name='annotations', on_delete=models.CASCADE)
-    annotations = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    date_posted = models.DateTimeField(default=datetime.now)
-
-
 class AdminUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     admin_name = models.TextField()
@@ -87,12 +47,43 @@ class ResidentUser(models.Model):
     residentName = models.CharField(max_length=200)
     floor = models.PositiveIntegerField()
 
-class resident_creates_ticket(models.Model):
-    resident_creates_ticket_id = models.AutoField(primary_key=True)
-    FK_task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    FK_resident = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    floor = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    ra = models.ForeignKey(RaUser, related_name='ra_tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    resident = models.ForeignKey(ResidentUser, related_name='resident_tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.IntegerField(
+        choices=[(1, "To Do"), (2, "In Progress"), (3, "On Hold"), (4, "Done")],
+        default=1,
+    )
+    priority = models.IntegerField(
+        choices=[(1, "Low"), (2, "Medium"), (3, "High")], default=1
+    )
+    date_posted = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comments(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    fk_task_comment = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_posted = models.DateTimeField(default=datetime.now)
 
 
+class Annotations(models.Model):
+    annotate_id = models.AutoField(primary_key=True)
+    fk_task_annotations = models.ForeignKey(Task, related_name='annotations', on_delete=models.CASCADE)
+    annotations = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_posted = models.DateTimeField(default=datetime.now)
+    
 class ChatMessage(models.Model):
     task = models.ForeignKey(Task, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
