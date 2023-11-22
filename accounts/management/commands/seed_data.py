fa7bc78437502f3e3c2f4b01a38f06c3497cddb5
Samuel Costantino <costan42@students.rowan.edu>
2023-11-21 23:15:41 -0500

    Added 1 new RA user and 1 new resident user

--- accounts/management/commands/seed_data.py
+++ accounts/management/commands/seed_data.py
@@ -48,6 +48,23 @@ class Command(BaseCommand):
 
         ra_user2.save()
 
+        # RAuser 3
+        
+        user = User.objects.create_user('orpisa38', 'orpisa38@rowan.edu', 'admin@123')
+
+        user.save()
+        
+        ra_user3 = RaUser(
+        user=user,
+        floor=2,
+        ra_name='Sadia Orpi',
+        room_number=101,
+        phone_number='123-456-7890',
+        ra_email= 'orpisa38@students.rowan.edu'
+        )
+
+        ra_user3.save()
+
         ###########################################################################
 
 
@@ -77,30 +94,43 @@ class Command(BaseCommand):
 
         resdient_user2.save()
 
+        #resident user 3
+        user= User.objects.create_user('costan42', 'costan42@rowan.edu', 'admin@123')
+
+        user.save()
+        
+        resdient_user3 = ResidentUser(
+        user = user,
+        residentName = "Samuel Costantino",
+        floor = 1 
+        )
+
+        resdient_user3.save()    
+
         Task.objects.create(title="Help With Cleaning room", floor=1, ra=ra_user, resident=resdient_user, description="I need an RA to come clean my room", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 1, 12, 0)))
         Task.objects.create(title="I need help with cleaning bathroom", floor=1, ra=ra_user,  resident=resdient_user, description="Bathroom needs to be cleaned", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 4, 20, 12, 0)))
         Task.objects.create(title="Bugs in my room ", floor=2, ra=ra_user2, resident=resdient_user2, description="Seems to be a bug infestation in my room", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 9, 15, 12, 0)))
         Task.objects.create(title="Outlet seems to be broken", floor=2, ra=ra_user2, resident=resdient_user2, description="My outlet seems to be broken", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 11, 20, 12, 0)))
-        Task.objects.create(title="The heater in my room broke", floor=3, ra=ra_user, resident=resdient_user, description="I sleep hot and my heater done broke", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 5, 12, 0)))
-        Task.objects.create(title="My AC makes a weird noise", floor=3, ra=ra_user, resident=resdient_user, description="I cannot sleep with the annoying sound my AC makes", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 10, 12, 0)))
-        Task.objects.create(title="The toilet's cloged", floor=4, ra=ra_user2, resident=resdient_user2, description="There is a clogged toilet on floor 4", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
-        Task.objects.create(title="The lightbulb in the bathroom died", floor=4, ra=ra_user2, resident=resdient_user2, description="I can't see what I'm doing in the bathroom", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
-        Task.objects.create(title="The shower only has cold water", floor=5, ra=ra_user, resident=resdient_user, description="Can't seem to get hot water from the shower", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
-        Task.objects.create(title="I lost my keys", floor=5, ra=ra_user, resident=resdient_user, description="Please let me know if you find them", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
-        Task.objects.create(title="I'm locked out of my room", floor=6, ra=ra_user2, resident=resdient_user2, description="I need to get back in my room please help", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 5, 12, 0)))
-        Task.objects.create(title="There's mold in my room", floor=6, ra=ra_user2, resident=resdient_user2, description="This is disgusting, I need to switch rooms", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 10, 12, 0)))
-        Task.objects.create(title="I found a pair of keys", floor=7, ra=ra_user, resident=resdient_user, description="Somebody missing their keys?", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
-        Task.objects.create(title="My bike got stolen", floor=7, ra=ra_user, resident=resdient_user, description="I'd like to report a missing bike", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
-        Task.objects.create(title="The bathroom sink is no longer working", floor=8, ra=ra_user2, resident=resdient_user2, description="bathroom sink on floor 8 is out of commision", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
-        Task.objects.create(title="My roomate is missing", floor=8, ra=ra_user2, resident=resdient_user2, description="I haven't seen or heard from the homie, I hope he safe", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
-        Task.objects.create(title="I need an extra key for my room", floor=9, ra=ra_user, resident=resdient_user, description="I lost my room key", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
-        Task.objects.create(title="Somebody spilled Pepsi in the lobby", floor=9, ra=ra_user, resident=resdient_user, description="Now the floor is all sticky", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
-        Task.objects.create(title="There is furniture missing in the lobby", floor=10, ra=ra_user2, resident=resdient_user2, description="I could've sworn there was a couch there", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
-        Task.objects.create(title="The lobby TV is broken", floor=10, ra=ra_user2, resident=resdient_user2, description="Now I can't have movie night with the homies", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
-        Task.objects.create(title="Missing an ID card", floor=1, ra=ra_user2, resident=resdient_user2, description="I can't get inside the building without my ID", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
-        Task.objects.create(title="The neigboors are too loud", floor=3, ra=ra_user, resident=resdient_user, description="How am I supposed to study like this?!", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
-        Task.objects.create(title="Eagles watch party, my place", floor=5, ra=ra_user, resident=resdient_user, description="Go Birds!!", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
-        Task.objects.create(title="I missplaced my notebook", floor=7, ra=ra_user2, resident=resdient_user2, description="Please let me know if you've found a missing notebook", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
-        Task.objects.create(title="Missing trash can", floor=9, ra=ra_user2, resident=resdient_user2, description="It is trash day and the trash can in nowhere to be found", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
+        Task.objects.create(title="The heater in my room broke", floor=3, ra=ra_user3, resident=resdient_user3, description="I sleep hot and my heater done broke", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 5, 12, 0)))
+        Task.objects.create(title="My AC makes a weird noise", floor=3, ra=ra_user3, resident=resdient_user3, description="I cannot sleep with the annoying sound my AC makes", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 10, 12, 0)))
+        Task.objects.create(title="The toilet's cloged", floor=1, ra=ra_user, resident=resdient_user, description="There is a clogged toilet on floor 4", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
+        Task.objects.create(title="The lightbulb in the bathroom died", floor=1, ra=ra_user, resident=resdient_user, description="I can't see what I'm doing in the bathroom", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
+        Task.objects.create(title="The shower only has cold water", floor=2, ra=ra_user2, resident=resdient_user2, description="Can't seem to get hot water from the shower", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
+        Task.objects.create(title="I lost my keys", floor=2, ra=ra_user2, resident=resdient_user2, description="Please let me know if you find them", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
+        Task.objects.create(title="I'm locked out of my room", floor=3, ra=ra_user3, resident=resdient_user3, description="I need to get back in my room please help", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 5, 12, 0)))
+        Task.objects.create(title="There's mold in my room", floor=3, ra=ra_user3, resident=resdient_user3, description="This is disgusting, I need to switch rooms", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 10, 12, 0)))
+        Task.objects.create(title="I found a pair of keys", floor=1, ra=ra_user, resident=resdient_user, description="Somebody missing their keys?", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
+        Task.objects.create(title="My bike got stolen", floor=1, ra=ra_user, resident=resdient_user, description="I'd like to report a missing bike", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
+        Task.objects.create(title="The bathroom sink is no longer working", floor=2, ra=ra_user2, resident=resdient_user2, description="bathroom sink on floor 8 is out of commision", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
+        Task.objects.create(title="My roomate is missing", floor=2, ra=ra_user2, resident=resdient_user2, description="I haven't seen or heard from the homie, I hope he safe", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
+        Task.objects.create(title="I need an extra key for my room", floor=3, ra=ra_user3, resident=resdient_user3, description="I lost my room key", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
+        Task.objects.create(title="Somebody spilled Pepsi in the lobby", floor=3, ra=ra_user3, resident=resdient_user3, description="Now the floor is all sticky", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
+        Task.objects.create(title="There is furniture missing in the lobby", floor=1, ra=ra_user, resident=resdient_user, description="I could've sworn there was a couch there", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
+        Task.objects.create(title="The lobby TV is broken", floor=1, ra=ra_user, resident=resdient_user, description="Now I can't have movie night with the homies", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
+        Task.objects.create(title="Missing an ID card", floor=2, ra=ra_user2, resident=resdient_user2, description="I can't get inside the building without my ID", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
+        Task.objects.create(title="The neigboors are too loud", floor=2, ra=ra_user2, resident=resdient_user2, description="How am I supposed to study like this?!", status=1, priority=1, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 15, 12, 0)))
+        Task.objects.create(title="Eagles watch party, my place", floor=3, ra=ra_user3, resident=resdient_user3, description="Go Birds!!", status=2, priority=2, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 20, 12, 0)))
+        Task.objects.create(title="I missplaced my notebook", floor=3, ra=ra_user3, resident=resdient_user3, description="Please let me know if you've found a missing notebook", status=3, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 25, 12, 0)))
+        Task.objects.create(title="Missing trash can", floor=1, ra=ra_user, resident=resdient_user, description="It is trash day and the trash can in nowhere to be found", status=4, priority=3, date_posted=timezone.make_aware(datetime.datetime(2023, 1, 30, 12, 0)))
 
         self.stdout.write(self.style.SUCCESS("Successfully seeded test data."))
